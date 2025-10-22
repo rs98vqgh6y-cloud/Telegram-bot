2eae762d96ae62da05a59849c748ce282681efef
rs98vqgh6y-cloud <rs98vqgh6y@privaterelay.appleid.com>
2025-10-22 14:26:02 +0200

    Update zakaz123.py

--- zakaz123.py
+++ zakaz123.py
@@ -3,7 +3,7 @@ import random
 import string
 import re
 from telegram import Update
-from telegram.ext import (ApplicationBuilder, CommandHandler, ContextTypesfrom, CallbackQueryHandler)
+from telegram.ext import (ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler)
 
 
 # Настройка логирования
@@ -282,6 +282,7 @@ def main():
 if __name__ == '__main__':
 
     main()
+
 
 
 
